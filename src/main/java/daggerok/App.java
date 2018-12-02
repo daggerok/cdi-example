482956c968fb458e447876d36e9d02e179772986
Maksim Kostromin <daggerok@gmail.com>
2018-12-03 01:32:06 +0200

    Cleanup...

--- src/main/java/daggerok/App.java
+++ src/main/java/daggerok/App.java
@@ -4,11 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.enterprise.inject.se.SeContainer;
 import javax.enterprise.inject.se.SeContainerInitializer;
+import javax.enterprise.inject.spi.BeanManager;
 
 @Slf4j
 public class App {
   public static void main(String[] args) {
-    SeContainer container = SeContainerInitializer.newInstance().initialize();
+    SeContainer container = SeContainerInitializer.newInstance()
+                                                  .setClassLoader(App.class.getClassLoader())
+                                                  .addPackages(App.class,
+                                                               BeanManager.class)
+                                                  .initialize();
     log.info("yo!");
   }
 }
