4ce9fbad8bd5df90a2ef816fc336be9e25e6d4ba
Maksim Kostromin <daggerok@gmail.com>
2019-09-01 23:58:33 +0300

    fix dependent and add more events | I am even stupider than I thought (C) whatthecommit.com

--- src/main/java/daggerok/events/App.java
+++ src/main/java/daggerok/events/App.java
@@ -4,8 +4,13 @@ import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
+import org.jboss.weld.environment.se.events.ContainerInitialized;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Initialized;
+import javax.enterprise.event.Event;
 import javax.enterprise.event.Observes;
+import javax.inject.Inject;
 
 @Value(staticConstructor = "of")
 class MyEvent {
@@ -20,6 +25,18 @@ class MyEventListener {
     }
 }
 
+@Slf4j
+@ApplicationScoped
+class MyEventProducer {
+
+    @Inject
+    private Event<MyEvent> events;
+
+    private void on(@Observes @Initialized(ApplicationScoped.class) ContainerInitialized event) {
+        events.fire(MyEvent.of("trololo-ololo..."));
+    }
+}
+
 public class App {
 
     public static void main(String[] args) {
@@ -28,7 +45,7 @@ public class App {
                                                  .initialize()) {
             container.event()
                      .select(MyEvent.class)
-                     .fire(MyEvent.of("ololo-trololo"));
+                     .fire(MyEvent.of("and ololo-trololo!"));
         }
     }
 }
